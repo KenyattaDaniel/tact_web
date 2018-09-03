0150e37909df5cc88a81e4cf2d3ca0d4ba595ef1
kenyatta <kenyatta@example.com>
2018-09-02 20:09:13 -0400

    Fix8

--- tactics/urls.py
+++ tactics/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url, include
+from django.contrib.staticfiles.urls import static
 
-from . import views
+from . import views, settings
+
 
 urlpatterns = [
     url(r'^$', views.tactics, name='tactics'),
     url(r'tactic/(?P<tactic_id>\d+)/$', views.tactic, name='tactic'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
